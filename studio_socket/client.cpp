17594f610b198b3ff331e315aa6e625af214c158
demian2435 <demian2435@gmail.com>
2021-06-07 11:24:47 +0200

    makefile

--- studio_socket/client.cpp
+++ studio_socket/client.cpp
@@ -20,17 +20,17 @@ int main(int argc, char **argv)
 
 	char buffer[1024] = {0};
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-		exit(1);
+		return(1);
 
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(PORT);
 	
 	// Convert IPv4 and IPv6 addresses from text to binary form
 	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
-		exit(1);
+		return(1);
 
 	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-		exit(1);
+		return(1);
 
 	send(sock , hello.c_str() , hello.length() , 0 );
 	std::cout << "Messaggio inviato al server" << std::endl;
