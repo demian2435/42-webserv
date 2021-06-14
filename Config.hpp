8e402073a4d211dfd74c64780b564970b3db4560
Andrea Duregon <aduregon@e3r9p7.42roma.it>
2021-06-14 17:42:45 +0200

    wewe

---
+++ Config.hpp
@@ -0,0 +1,255 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Config.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aduregon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/14 12:33:40 by aduregon          #+#    #+#             */
+/*   Updated: 2021/06/14 17:41:48 by aduregon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <list>
+#include <string>
+#include <iostream>
+#include <list>
+#include <vector>
+#include <fstream>
+
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
+std::vector<std::string> split(std::string s, std::string del = " ")
+	{
+		std::vector<std::string> out;
+	    int start = 0;
+	    int end = s.find(del);
+	    while (end != -1) {
+	        out.push_back(s.substr(start, end - start));
+	        start = end + del.size();
+	        end = s.find(del, start);
+	    }
+	    out.push_back(s.substr(start, end - start));
+		std::vector<std::string> ret;
+		for (size_t i = 0; i < out.size(); i++)
+			ret.push_back(out[i]);
+		
+		return ret;
+	}
+
+class ErrorPages
+{
+private:
+	int         key[700];
+	std::string path[700];
+public:
+	ErrorPages()
+	{
+		for (size_t i = 0; i < 700; i++)
+		{
+			key[i] = i;
+			path[i] = "";
+		}
+	}
+
+	~ErrorPages(){}
+	
+	void    insertPath(int code, std::string path)
+	{
+		this->path[code] = path;
+	}
+
+	std::string getPath(int code)
+	{
+		return this->path[code];
+	}
+};
+
+class Location
+{
+public:
+	std::string                     path;
+    std::string                     root;
+    std::vector<std::string>        index;
+    //std::list<std::string>          method;
+    std::string                     cgi_path;
+    std::string                     cgi_extension;
+    bool                            autoindex;
+    size_t                          client_max_body_size;
+
+public:
+	Location(std::ifstream &file, std::string path)
+	{
+		std::string					buff;
+		std::string					line;
+		std::vector<std::string>	parse;
+		int							i;
+		
+		this->path = path;
+		while (getline(file, buff))
+		{
+			i = 0;
+			line = "";
+			while (isspace(buff[i]))
+				i++;
+			while (buff[i] != ';' && buff[i] != '#' && buff[i])
+			{
+				line += buff[i];
+				i++;
+			}
+			parse = split(line, " ");
+			if (!parse[0].compare("root") && parse.size() == 2)
+				this->root = parse[1];
+			if (!parse[0].compare("cgi_path") && parse.size() == 2)
+				this->cgi_path = parse[1];
+			if (!parse[0].compare("cgi_extension") && parse.size() == 2)
+				this->cgi_extension = parse[1];
+			if (!parse[0].compare("client_max_body_size") && parse.size() == 2)
+			{
+				this->client_max_body_size = std::stof(parse[1].c_str());
+			}
+			if (!parse[0].compare("autoindex") && parse.size() == 2)
+			{
+				if(!parse[1].compare("on"))
+					this->autoindex = true;
+				else
+					this->autoindex = false;
+			}
+			if (!parse[0].compare("index") && parse.size() > 1)
+			{
+				for (size_t k = 1; k < parse.size(); k++)
+					this->index.push_back(parse[k]);
+			}
+			if (!parse[0].compare("}"))
+				break ;
+			line.clear();
+		}
+	}
+	~Location() {}
+};
+
+bool	compare_location(Location const &s1, Location const &s2)
+{
+	return (s1.path.size() > s2.path.size());
+}
+
+class Server
+{
+public:
+	std::string             	host;
+    int			           		port;
+    std::string             	name;
+    ErrorPages              	error_pages;
+	std::vector<Location>		location;
+public:
+	Server(std::ifstream &file)
+	{
+		std::string					buff;
+		std::string					line;
+		std::vector<std::string>	parse;
+		int			i;
+		this->host = "127.0.0.1";
+		//this->port = 80;
+		while (getline(file, buff))
+		{
+			i = 0;
+			while (isspace(buff[i]))
+				i++;
+			while (buff[i] != ';' && buff[i] != '#' && buff[i])
+			{
+				line += buff[i];
+				i++;
+			}
+			parse = split(line, " ");
+			if (!parse[0].compare("listen") && parse.size() == 2)
+			{
+				//this->host = parse[1];
+				this->port = std::stoi(parse[1].c_str());
+			}
+			if (!parse[0].compare("error_page") && parse.size() == 3)
+				error_pages.insertPath(std::stoi(parse[1].c_str()), parse[2]);
+			if (!parse[0].compare("server_name") && parse.size() == 2)
+				this->name = parse[1];
+			if (!parse[0].compare("location") && parse.size() == 3)
+			{
+				this->location.push_back(Location(file, parse[1]));
+			}
+			if (!parse[0].compare("}"))
+			{
+				std::sort(this->location.begin(), this->location.end(), compare_location);
+				if (this->location.size() == 0)
+				{
+					std::cout << BOLDRED << "Error: no default location '/'\n" << RESET;
+					exit(-1);
+				}
+				break ;
+			}
+			line = "";
+			for (size_t y = 0; y < parse.size(); y++)
+				parse[y].clear();
+			
+		}
+	}
+	
+	~Server() {}
+};
+
+class Config
+{
+public:
+	std::vector<Server>	server;
+public:
+	Config(std::string path = "./webserv.conf")
+	{
+		std::string					buff;
+		std::string					line;
+		std::vector<std::string>	parse;
+		int			i;
+		std::ifstream				file(path);
+		
+		if (!file.is_open())
+		{
+			std::cout << BOLDRED << "Error: can't open config file\n" << RESET;
+			exit(-1);
+		}
+		while (getline(file, buff))
+		{
+			i = 0;
+			while (isspace(buff[i]))
+				i++;
+			while (buff[i] != ';' && buff[i] != '#' && buff[i] != '{')
+			{
+				line += buff[i];
+				i++;
+			}
+			parse = split(line, " ");
+			if (!parse[0].compare("server"))
+			{
+				this->server.push_back(Server(file));
+			}
+			line = "";
+		}
+	}
+
+	~Config() {}
+};
+
--- main.cpp
+++ main.cpp
@@ -1,41 +1,38 @@
-#include "studio_socket/conf_parsing.hpp"
+#include "Config.hpp"
 #include "studio_socket/Request.hpp"
 #include "studio_socket/Response.hpp"
-config    test_config()
+Config    test_config()
 {
-    config  info;
+	Config conf("./studio_socket/webserv.conf");
 
-    // std::cout << info.name << std::endl;
-    // std::cout << info.host << std::endl;
-    // std::cout << info.port << std::endl;
-    // std::cout << "error pages:" << std::endl;
-    // for (size_t i = 0; i < 700; i++)
-    // {
-    //     if (info.error_pages.getPath(i) != "")
-    //         std::cout << "  " << i << " " << info.error_pages.getPath(i) << std::endl;
-    // }
-    // std::list<location>::iterator    it(info.locations.begin());
-    // std::list<std::string>::iterator   it2;
-
-    // while (it != info.locations.end())
-    // {
-    //     std::cout << "path: " << it->getPath() << std::endl;
-    //     std::cout << "  root: " << it->getRoot() << std::endl;
-    //     std::cout << "  autoindex: " << it->getAutoindex() << std::endl;
-    //     std::cout << "  cgi_extension: " << it->getCgiExtension() << std::endl;
-    //     it2 = it->getIndex().begin();
-    //     std::cout << "  Index: " << std::endl;
-    //     for (size_t i = 0; i < it->getIndex().size(); i++)
-    //     {
-    //         std::cout << "      " << *it2 << std::endl;
-    //         it2++;
-    //     }
-        
-    //     std::cout << "  cgi_path:" << it->getCgiPath() << std::endl;
-    //     std::cout << "  max body size: " << it->getMaxBodySize() << std::endl;
-    //     it++;
-    // }
-    return info;
+	for (size_t i = 0; i < conf.server.size(); i++)
+	{
+		if (i %2 == 0)
+			std::cout << RED;
+		else
+			std::cout << YELLOW;
+		std::cout << "HOST: " << conf.server[i].host << std::endl;
+		std::cout << "NAME: " << conf.server[i].name << std::endl;
+		std::cout << "PORT: " << conf.server[i].port << std::endl;
+		for (size_t j = 0; j < 700; j++)
+		{
+			//puts("rrrrrrr");
+			if (conf.server[i].error_pages.getPath(j) != "")
+				std::cout << "ERROR: " << j << " " << conf.server[i].error_pages.getPath(j) << std::endl;
+		}
+		for (size_t j = 0; j < conf.server[i].location.size(); j++)
+		{
+			std::cout << "	PATH: " << conf.server[i].location[j].path << std::endl;
+			std::cout << "	ROOT: " << conf.server[i].location[j].root << std::endl;
+			std::cout << "	CGI-PATH: " << conf.server[i].location[j].cgi_path << std::endl;
+			std::cout << "	CGI-EXTENSION: " << conf.server[i].location[j].cgi_extension << std::endl;
+			std::cout << "	MAX BODY: " << conf.server[i].location[j].client_max_body_size << std::endl;
+			for (size_t x = 0; x < conf.server[i].location[j].index.size(); x++)
+				std::cout << "		INDEX: " << conf.server[i].location[j].index[x] << std::endl;	
+		}
+		std::cout << RESET;	
+	}
+	return conf;
 }
 
 Request request_test()
@@ -48,11 +45,13 @@ Request request_test()
 
 int main()
 {
-    config c;
-    Request r;
-    c = test_config();
-    r = request_test();
-    Response tmp(c, r);
-    std::cout << tmp.out;
+	// std::string str = "ciao belol beololvoflvc";
+	test_config();
+    // config c;
+    // Request r;
+    // c = test_config();
+    // r = request_test();
+    // Response tmp(c, r);
+    // std::cout << tmp.out;
 
 }
