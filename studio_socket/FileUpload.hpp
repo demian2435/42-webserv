f02a2cc37045e879055ab7d64a3a22941381980f
Andrea Duregon <aduregon@e3r9p7.42roma.it>
2021-06-17 18:45:44 +0200

    üpload

--- studio_socket/FileUpload.hpp
+++ studio_socket/FileUpload.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   FileUpload.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dmalori <dmalori@student.42.fr>            +#+  +:+       +#+        */
+/*   By: aduregon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 15:03:15 by aduregon          #+#    #+#             */
-/*   Updated: 2021/06/17 16:38:56 by dmalori          ###   ########.fr       */
+/*   Updated: 2021/06/17 18:43:05 by aduregon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <iostream>
 #include <fstream>
 #include <chrono>
+#include <sstream>
 
 class FileUpload
 {
@@ -23,26 +24,59 @@ private:
 public:
 	FileUpload(/* args */) {}
 
-	FileUpload(std::string body)
+	FileUpload(Request req)
 	{
 		std::string name;
 		std::chrono::time_point<std::chrono::system_clock> p1 = std::chrono::system_clock::now();
- 
-		while (1)
+		if (!(req.transfer_encoding.compare(0, 7, "chunked")))
+		{
+			long int time = std::chrono::duration_cast<std::chrono::milliseconds>(p1.time_since_epoch()).count();
+			name = "file_" + std::to_string(time);
+			std::ofstream file("./upload/" + name);
+			int	size = 0;
+			for (size_t i = 0; i < req.body.length(); i++)
+			{
+				std::string tmp;
+				// 13 == \r
+				while (req.body[i] != 13)
+				{
+					tmp += req.body[i];
+					i++;
+				}
+				size = htoi(tmp);
+				if (!size)
+					break ;
+				i += 2 ;
+				for (size_t j = 0; j < (size_t)size; j++)
+				{
+					file << req.body[i];
+					i++;
+				}
+			}
+			file.close();
+		}
+		else
 		{
 			long int time = std::chrono::duration_cast<std::chrono::milliseconds>(p1.time_since_epoch()).count();
 			name = "file_" + std::to_string(time);
 			std::ifstream file("./upload/" + name);
-
 			if (!file.is_open())
 			{
 				std::ofstream file("./upload/" + name);
-				file << body;
-				break ;
+				file << req.body;
 			}
-			else
-				continue ;
+			file.close();
 		}
+	}
+
+	int		htoi(std::string hex)
+	{
+		std::stringstream x;
+		int res;
+		
+		x << std::hex << hex;
+		x >> res;
+		return res;
 	}
 
 	~FileUpload() {}
